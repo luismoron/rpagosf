d48ac9b1bc6b5547d792b19c6568d1b5470e0124
Luis Morón <luisleonelmoron@gmail.com>
2025-06-21 09:39:05 -0400

    Add initial Flutter project structure with assets and launch screens
    - Created iOS assets for app icons in various sizes.
    - Added launch images for different resolutions.
    - Configured launch screen storyboard and main storyboard.
    - Updated Info.plist with necessary configurations.
    - Added bridging header for Swift compatibility.
    - Included initial test cases for the Flutter application.
    - Set up pubspec.yaml with dependencies and project metadata.

---
+++ android/app/src/main/kotlin/com/tecnoyotabytes/rpago/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tecnoyotabytes.rpago
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Inicio')),
+      body: const Center(child: Text('¡Bienvenido a tu nueva app Flutter!')),
+    );
+  }
+}
